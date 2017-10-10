fd509055197014be36a0bca865dd4f0691debc82
pontuslaos <pontus.laos@lund.ntig.se>
2017-10-10 13:47:01 +0200

    Added tables for users and history, also altered books table to keep track of borrower.

--- db/library.sql
+++ db/library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 10 okt 2017 kl 11:56
+-- Tid vid skapande: 10 okt 2017 kl 13:46
 -- Serverversion: 10.1.19-MariaDB
 -- PHP-version: 7.0.13
 
@@ -53,7 +53,20 @@ CREATE TABLE `books` (
   `isbn-13` varchar(13) NOT NULL DEFAULT 'NONE',
   `title` varchar(100) DEFAULT NULL,
   `language` varchar(50) DEFAULT NULL,
-  `release_year` varchar(25) DEFAULT NULL
+  `release_year` varchar(25) DEFAULT NULL,
+  `borrower_social_secuirty_number` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `history`
+--
+
+CREATE TABLE `history` (
+  `social_security_number` varchar(13) NOT NULL,
+  `isbn-10` int(10) NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -64,6 +77,18 @@ CREATE TABLE `books` (
 
 CREATE TABLE `registered_books` (
   `isbn-10` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+CREATE TABLE `users` (
+  `social_security_number` varchar(13) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -89,10 +114,22 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`isbn-10`);
 
 --
+-- Index för tabell `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`social_security_number`,`isbn-10`,`date`);
+
+--
 -- Index för tabell `registered_books`
 --
 ALTER TABLE `registered_books`
   ADD PRIMARY KEY (`isbn-10`);
+
+--
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`social_security_number`);
 
 --
 -- AUTO_INCREMENT för dumpade tabeller
@@ -102,7 +139,7 @@ ALTER TABLE `registered_books`
 -- AUTO_INCREMENT för tabell `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
