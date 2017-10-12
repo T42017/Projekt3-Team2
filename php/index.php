076ef7827487a77256481da6c38ace2869422fee
pontuslaos <pontus.laos@lund.ntig.se>
2017-10-12 14:09:48 +0200

    Removed index.css from use, added book covers to table. Fixed some bootstrap.

--- php/index.php
+++ php/index.php
@@ -4,6 +4,8 @@ define("path", realpath(__DIR__ . '/..'));
 
 require path . '/vendor/autoload.php';
 require 'dbconnection.php';
+require 'helpers.php';
+
 $loader = new Twig_Loader_Filesystem(path . '/templates');
 $twig = new Twig_Environment($loader, array(
 	'cache' => path . '/cache', 
