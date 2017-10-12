3ea27db0bc4832c1c601618b8c3344985b3dd2a0
pontuslaos <pontus.laos@lund.ntig.se>
2017-10-12 14:11:13 +0200

    Removed unused require statement in index.php introduced in latest commit.

--- php/index.php
+++ php/index.php
@@ -4,7 +4,6 @@ define("path", realpath(__DIR__ . '/..'));
 
 require path . '/vendor/autoload.php';
 require 'dbconnection.php';
-require 'helpers.php';
 
 $loader = new Twig_Loader_Filesystem(path . '/templates');
 $twig = new Twig_Environment($loader, array(
