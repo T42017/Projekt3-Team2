a08afabff4e337518e5bc6972d0ead78fb301ecf
Måns H <mans.holmgren@lund.ntig.se>
2017-10-13 11:01:33 +0200

    Develop 2.0

--- php/index.php
+++ php/index.php
@@ -1,20 +1,33 @@
 <?php
+    define("path", realpath(__DIR__ . '/..'));
+    
+	require path . '/vendor/autoload.php';
+	$loader = new Twig_Loader_Filesystem(path . '/templates');
+	$twig = new Twig_Environment($loader, array('cache' => path . '/cache', 'debug' => true));
+	
+    $libraryPDO = new PDO('mysql:host=localhost;dbname=library;charset=utf8mb4', 'root', '');
 
-define("path", realpath(__DIR__ . '/..'));
-
-require path . '/vendor/autoload.php';
-require 'dbconnection.php';
-
-$loader = new Twig_Loader_Filesystem(path . '/templates');
-$twig = new Twig_Environment($loader, array(
-	'cache' => path . '/cache', 
-	'debug' => true)
-);
-
-$allbooks = $db->query('SELECT * FROM books')->fetchAll();
-
-echo $twig->render('index.twig', array(
-	'allbooks' => $allbooks
-));
-
+    if(isset($_GET['search']))
+	{
+        $search = urldecode($_GET['search']);
+        //$param = htmlspecialchars(str_replace('+', ' ', $_GET['search']));
+		$stmt = $libraryPDO->prepare("SELECT * FROM books WHERE title = :name ");
+        $specialparam = '%'.$search;
+        
+        var_dump($specialparam);
+        
+		$stmt->bindParam(':name', $search); 
+		$stmt->execute();
+        
+        
+        
+        echo $twig->render('index.twig', 
+		array(
+			'names' => $stmt->fetchAll(),
+			));
+	}
+	else
+	{
+		echo $twig->render('index.twig');
+	}
 ?>
