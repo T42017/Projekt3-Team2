02920a12239872c250daa03f8b9ae7bb90b192e5
pontuslaos <pontus.laos@lund.ntig.se>
2017-10-13 11:19:42 +0200

    Made use of dbconnection.php in index.php. Also made the logo link to index.php, though the link is offset for some reason.

--- php/index.php
+++ php/index.php
@@ -2,24 +2,20 @@
     define("path", realpath(__DIR__ . '/..'));
     
 	require path . '/vendor/autoload.php';
+	require 'dbconnection.php';
+
 	$loader = new Twig_Loader_Filesystem(path . '/templates');
 	$twig = new Twig_Environment($loader, array('cache' => path . '/cache', 'debug' => true));
 	
-    $libraryPDO = new PDO('mysql:host=localhost;dbname=library;charset=utf8mb4', 'root', '');
-
     if(isset($_GET['search']))
 	{
         $search = urldecode($_GET['search']);
         //$param = htmlspecialchars(str_replace('+', ' ', $_GET['search']));
-		$stmt = $libraryPDO->prepare("SELECT * FROM books WHERE title = :name ");
+		$stmt = $db->prepare("SELECT * FROM books WHERE title = :name ");
         $specialparam = '%'.$search;
-        
-        var_dump($specialparam);
         
 		$stmt->bindParam(':name', $search); 
 		$stmt->execute();
-        
-        
         
         echo $twig->render('index.twig', 
 		array(
@@ -28,6 +24,9 @@
 	}
 	else
 	{
-		echo $twig->render('index.twig');
+		$stmt = $db->query('SELECT * FROM books');
+		echo $twig->render('index.twig', array(
+			'names' => $stmt->fetchAll()
+		));
 	}
 ?>
